4a4f7bfa08fcd6693c87f23a41929ccdfc2abe14
Pedro Mendes <pedrito.kikas@gmail.com>
2023-06-15 20:00:09 +0100

    add block based on stack size

--- StackDyn/src/Console/Console.cpp
+++ StackDyn/src/Console/Console.cpp
@@ -42,39 +42,42 @@ void Console::Init()
             crane->addToHistory(newBlock, "null");
             break; // add to history
         }
-
-
-        // Add the block to the arrival stack
-        arrivalStack->AddBlock(newBlock);
-        crane->addToHistory(newBlock, "arrival");
-
-        //std::cout << "New block created and added to the arrival stack. Block ID: " << blockId << std::endl;
-
-        // Move one container from the arrival stack to a stack buffer
-        if (buffer1.hasSpace()) {
-            Block* block = arrivalStack->RemoveBlock();  // Remove a block from the arrival stack
-            buffer1.AddBlock(block);  // Move the block to buffer1
-            crane->addToHistory(newBlock, "buffer 1");
-            buffer1.PrintBlocks();
+        else {
+            // Add the block to the arrival stack
+            arrivalStack->AddBlock(newBlock);
+            crane->addToHistory(newBlock, "arrival");
+            std::cout << "New Block with ID: " << newBlock->GetId() << " added" << std::endl;
+            std::cout << "" << std::endl;
         }
-        else if (buffer2.hasSpace()) {
-            Block* block = arrivalStack->RemoveBlock();  // Remove a block from the arrival stack
-            buffer2.AddBlock(block);  // Move the block to buffer2
-            crane->addToHistory(newBlock, "buffer 2");
-            buffer2.PrintBlocks();
+
+
+        // Move the block to the buffer with the least amount of blocks
+        Stack* targetBuffer = nullptr;
+        int minBufferSize = INT_MAX;
+
+        if (buffer1.hasSpace() && buffer1.GetSize() < minBufferSize) {
+            targetBuffer = &buffer1;
+            minBufferSize = buffer1.GetSize();
         }
-        else if (buffer3.hasSpace()) {
-            Block* block = arrivalStack->RemoveBlock();  // Remove a block from the arrival stack
-            buffer3.AddBlock(block);  // Move the block to buffer3
-            crane->addToHistory(newBlock, "buffer 3");
-            buffer3.PrintBlocks();
+
+        if (buffer2.hasSpace() && buffer2.GetSize() < minBufferSize) {
+            targetBuffer = &buffer2;
+            minBufferSize = buffer2.GetSize();
         }
-        else if (handover.hasSpace()) {
+
+        if (buffer3.hasSpace() && buffer3.GetSize() < minBufferSize) {
+            targetBuffer = &buffer3;
+            minBufferSize = buffer3.GetSize();
+        }
+
+        if (targetBuffer != nullptr) {
             Block* block = arrivalStack->RemoveBlock();  // Remove a block from the arrival stack
-            handover.AddBlock(block);  // Move the block to buffer3
-            handover.PrintBlocks();
-            crane->addToHistory(newBlock, "delivered");
-            handover.RemoveBlock();
+            targetBuffer->AddBlock(block);  // Move the block to the target buffer
+            crane->addToHistory(newBlock, "buffer");
+            targetBuffer->PrintBlocks();
+        }
+        else {
+            std::cout << "No buffer available. Cannot move block." << std::endl;
         }
 
         blockId++;
